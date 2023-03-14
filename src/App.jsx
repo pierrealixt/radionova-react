a0f4e0499e62a2d7c97fe68543509e7c309e7882
Pierre-Alix Tremblay <pierrealix@icloud.com>
2023-03-14 22:04:51 +0200

    appwrite integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import { createRoot } from "react-dom/client";
+import { Account, Client, Databases } from "appwrite";
 
 const Radio = (props) => {
-  const [track, setTrack] = React.useState(null);
-  React.useEffect(() => {
+  const [track, setTrack] = useState(null);
+  useEffect(() => {
     async function fetchTrack () {
       const response = await fetch(`https://www.nova.fr/wp-json/radios/${props.name}`)
       const data = await response.json()
@@ -21,12 +22,32 @@ const Radio = (props) => {
         style={{
           margin: "2rem 0"
         }}
-        onSubmit={(e) => {
+        onSubmit={async(e) => {
           e.preventDefault()
           const formData = new FormData(e.target)
           for (const entry of formData.entries()) {
             console.log(entry)
           }
+
+
+const client = new Client()
+          .setEndpoint('https://appwrite.pierrealix.fr/v1')
+            .setProject('6410a2a175f66668f520');
+const account = new Account(client);
+await account.createEmailSession('myradionova@pierrealix.fr', 'appwrite-myradionova');
+
+const databases = new Databases(client);
+
+await databases.createDocument(
+    '6410a34e2c9775636d92',
+    '6410a3542de87cf5ccbf',
+    crypto.randomUUID(),
+    {
+      artist: formData.get('artist'),
+      title: formData.get('title'),
+      youtube_id: formData.get('youtube_id')
+    }
+); 
         }}
       >
         <fieldset         style={{
