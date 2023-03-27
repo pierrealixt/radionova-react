850334874ce55641abae8ed16a90698155a92f8b
Pierre-Alix Tremblay <pierrealix@icloud.com>
2023-03-27 21:40:57 +0200

    track form

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,91 @@
-import { StrictMode, useState, useEffect } from "react";
+import { StrictMode, useState, useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
-import { Account, Client, Databases } from "appwrite";
+import { Client, Databases, Query } from "appwrite";
 
-const Radio = (props) => {
+const database = () => {
+  const client = new Client();
+  client
+    .setEndpoint("https://appwrite.pierrealix.fr/v1")
+    .setProject("6410a2a175f66668f520");
+  return new Databases(client);
+};
+
+const Track = ({ track, nextTrack }) => {
+  const ref = useRef(null);
+  return (
+    <form
+      ref={ref}
+      onSubmit={async (event) => {
+        event.preventDefault();
+        const formData = new FormData(ref.current);
+        console.log(formData.get("track_id"));
+        console.log(formData.get("youtube_id"));
+
+        const result = await database().updateDocument(
+          "6410a34e2c9775636d92",
+          "6410a3542de87cf5ccbf",
+          formData.get("track_id"),
+          {
+            youtube_id: formData.get("youtube_id"),
+          }
+        );
+        if (result) {
+          nextTrack();
+        }
+      }}
+    >
+      <input type="hidden" name="track_id" defaultValue={track.$id} />
+      <input type="text" defaultValue={track.artist} />
+      <input type="text" defaultValue={track.title} />
+      <a
+        target="_blank"
+        href={`https://google.com/search?q=youtube ${track.artist} ${track.title}`}
+      >
+        google
+      </a>
+      <input type="text" name="youtube_id" />
+      <input type="submit" />
+    </form>
+  );
+};
+
+const App = () => {
   const [track, setTrack] = useState(null);
   useEffect(() => {
     async function fetchTrack() {
-      const response = await fetch(
-        `https://www.nova.fr/wp-json/radios/${props.name}`
-      );
-      const data = await response.json();
-      if (data.currentTrack) {
-        setTrack(data.currentTrack);
-      }
+      const list = async (cursor) => {
+        const queries = [Query.limit(1)];
+        if (cursor) {
+          queries.push(Query.cursorAfter(cursor));
+        }
+        return await database().listDocuments(
+          "6410a34e2c9775636d92",
+          "6410a3542de87cf5ccbf",
+          queries
+        );
+      };
+
+      const loop = async (cursor) => {
+        const result = await list(cursor);
+        if (!result.documents[0].youtube_id) {
+          return result.documents[0];
+        }
+
+        return await loop(result.documents[result.documents.length - 1].$id);
+      };
+
+      const track = await loop(null);
+      setTrack(track);
     }
 
-    fetchTrack();
-  }, []);
-  return (
-    <>
-      {track && (
-        <form
-          style={{
-            margin: "2rem 0",
-          }}
-          onSubmit={async (e) => {
-            e.preventDefault();
-            const formData = new FormData(e.target);
-            for (const entry of formData.entries()) {
-              console.log(entry);
-            }
-
-            const client = new Client()
-              .setEndpoint("https://appwrite.pierrealix.fr/v1")
-              .setProject("6410a2a175f66668f520");
-            const account = new Account(client);
-            await account.createEmailSession(
-              "myradionova@pierrealix.fr",
-              "appwrite-myradionova"
-            );
-
-            const databases = new Databases(client);
-
-            await databases.createDocument(
-              "6410a34e2c9775636d92",
-              "6410a3542de87cf5ccbf",
-              crypto.randomUUID(),
-              {
-                artist: formData.get("artist"),
-                title: formData.get("title"),
-                youtube_id: formData.get("youtube_id"),
-              }
-            );
-          }}
-        >
-          <fieldset
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-            }}
-          >
-            <legend>{props.name}</legend>
-            <input type="text" name="artist" defaultValue={track.artist} />
-            <input type="text" name="title" defaultValue={track.title} />
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                window.open(
-                  `https://google.com/search?q=youtube+${track.artist}+${track.title}`
-                );
-              }}
-            >
-              youtube
-            </button>
-            <input type="text" placeholder="youtube id" name="youtube_id" />
-            <input type="submit" value="submit" />
-          </fieldset>
-        </form>
-      )}
-    </>
-  );
-};
-const App = () => {
+    if (track == null) {
+      fetchTrack();
+    }
+  });
   return (
     <StrictMode>
-      <Radio name="nova-soul" />
-      <Radio name="nova-la-nuit" />
-      <Radio name="nova-classics" />
-      <Radio name="nouvo-nova" />
+      {track && <Track track={track} nextTrack={() => setTrack(null)} />}
+      {track == null && <p>Loading...</p>}
     </StrictMode>
   );
 };
